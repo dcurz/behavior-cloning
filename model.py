e1e5233577cd8be3937d6179e29e7b410a9c15f7
David Chrzanowski <chrzanowski.david@gmail.com>
2017-07-20 22:35:09 -0400

    Add pixel normalization and image cropping

--- model.py
+++ model.py
@@ -15,8 +15,6 @@ with open('./driving_log.csv') as csvfile:
 from sklearn.model_selection import train_test_split
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
-print(samples)
-
 def generator(samples, batch_size=32):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
@@ -26,13 +24,6 @@ def generator(samples, batch_size=32):
 
             images = []
             angles = []
-            
-            #for batch_sample in batch_samples:
-                #name = './IMG/'+batch_sample[0].split('/')[-1]
-                #center_image = cv2.imread(name)
-                #center_angle = float(batch_sample[3])
-                #images.append(center_image)
-                #angles.append(center_angle)
 
             for line in batch_samples:
                 source_path = line[0]
@@ -43,7 +34,6 @@ def generator(samples, batch_size=32):
                 images.append(image)
                 angles.append(float(line[3]))
 
-            # trim image to only see section with road
             X_train = np.array(images)
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)
@@ -52,19 +42,19 @@ def generator(samples, batch_size=32):
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
-#ch, row, col = 3, 65, 320  # Trimmed image format
+ch, row, col = 3, 65, 320  # Trimmed image format
 
 
 model = Sequential()
 # Preprocess incoming data, centered around zero with small standard deviation 
-#model.add(Cropping2D(cropping=((70,25),(0,0)), input_shape=(160,320,3)))
-#model.add(Lambda(lambda x: x/127.5 - 1.,
-#        input_shape=(row, col, ch),
-#        output_shape=(row, col, ch)))
-model.add(Flatten(input_shape=(160,320,3)))
+#model.add(Cropping2D(cropping=((70,25),(0,0))))
+model.add(Lambda(lambda x: (x/255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25),(0,0))))
+model.add(Flatten())
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
 
 model.save('model.h5')
+print("model saved")
